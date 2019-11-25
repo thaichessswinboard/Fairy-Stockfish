596ce5aed0a8da90c8d1b488a11a36a56088a3d6
Fabian Fichter <ianfab@users.noreply.github.com>
2019-11-25 14:11:18 +0100

    tune mobility

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -111,7 +111,9 @@ namespace {
       S( 79,140), S( 88,143), S( 88,148), S( 99,166), S(102,170), S(102,175),
       S(106,184), S(109,191), S(113,206), S(116,212) }
   };
-  constexpr Score MaxMobility  = S(250, 250);
+  Score MaxMobility  = S(250, 250);
+  int m[2]  = {1, 8};
+  TUNE(SetRange(0, 1000), MaxMobility, SetRange(0, 30), m);
   constexpr Score DropMobility = S(10, 10);
 
   // RookOnFile[semiopen/open] contains bonuses for each rook when there is
@@ -335,7 +337,7 @@ namespace {
         if (Pt <= QUEEN)
             mobility[Us] += MobilityBonus[Pt - 2][mob];
         else
-            mobility[Us] += MaxMobility * (mob - 1) / (8 + mob);
+            mobility[Us] += MaxMobility * (mob - m[0]) / (m[1] + mob);
 
         // Piece promotion bonus
         if (pos.promoted_piece_type(Pt) != NO_PIECE_TYPE)
