1a9e31a43c6747e087f987565f511d02951da77a
Fabian Fichter <ianfab@users.noreply.github.com>
2019-11-26 21:52:15 +0100

    Tune piece values

--- src/psqt.cpp
+++ src/psqt.cpp
@@ -23,6 +23,7 @@
 #include "types.h"
 #include "piece.h"
 #include "variant.h"
+#include "uci.h"
 
 Value PieceValue[PHASE_NB][PIECE_NB] = {
   { VALUE_ZERO, PawnValueMg, KnightValueMg, BishopValueMg, RookValueMg, QueenValueMg,
@@ -170,5 +171,15 @@ void init(const Variant* v) {
       psq[~pc][SQ_NONE] = -psq[pc][SQ_NONE];
   }
 }
+
+void init_cur() { init(variants.find(Options["UCI_Variant"])->second); }
+
+TUNE(SetRange(0, 5000),
+     PieceValue[MG][PAWN], PieceValue[EG][PAWN],
+     PieceValue[MG][KNIGHT], PieceValue[EG][KNIGHT],
+     PieceValue[MG][BISHOP], PieceValue[EG][BISHOP],
+     PieceValue[MG][ROOK], PieceValue[EG][ROOK],
+     PieceValue[MG][QUEEN], PieceValue[EG][QUEEN],
+     init_cur);
 
 } // namespace PSQT
